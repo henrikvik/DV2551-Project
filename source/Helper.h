6e16cab7f8b8a0fa5f6b16d319cde9706266d6cc
Henrik Vik <henkvik@gmail.com>
2018-02-23 11:34:15 +0100

    added break on fail

--- source/Helper.h
+++ source/Helper.h
@@ -4,6 +4,27 @@
 
 #define SafeRelease(ptr) {if(ptr){ptr->Release();ptr=nullptr;}}
 
+#define BreakOnFail(hr)  {\
+    if FAILED(hr) \
+    { \
+        OutputDebugString(_bstr_t("\n---==+==---\n"));\
+        OutputDebugString(_com_error(hr).ErrorMessage());\
+        OutputDebugString(_bstr_t("\n---==+==---\n"));\
+        _CrtDbgBreak();\
+    }\
+}
+
+#define BreakOnFail1(hr, message)\
+{\
+    if FAILED(hr)\
+    {\
+        OutputDebugString(_bstr_t("\n---==+==---\n"));\
+        OutputDebugString(_bstr_t(message));\
+        OutputDebugString(_bstr_t("\n---==+==---\n"));\
+        _CrtDbgBreak();\
+    }\
+}
+
 
 struct G
 {
